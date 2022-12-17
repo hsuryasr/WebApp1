00ce4dbef18753f853aaf92d4899ed7ac5fc8f45
hsuryasr <115404699+hsuryasr@users.noreply.github.com>
2022-12-17 13:12:22 +0000

    Add files via upload

---
+++ ProductController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Index()
+        {
+            Models.Product p = new Models.Product();
+            return View(p.GetProducts());
+        }
+    }
+}
